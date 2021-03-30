8d715aaf6187b4799eb6d2ae8a9bfd69c2879cb5
cheverriaw <willytpi2021@gmail.com>
2021-03-29 23:36:22 -0600

    iniciando la prueba para reservaDomain

---
+++ src/test/java/com/ues/edu/sv/modelo/ReservaDomainTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ues.edu.sv.modelo;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author william
+ */
+public class ReservaDomainTest extends TestCase {
+    
+    public ReservaDomainTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of getLibro method, of class ReservaDomain.
+     */
+    public void testGetLibro() {
+        System.out.println("getLibro");
+        ReservaDomain instance = new ReservaDomain();
+        LibroDomain expResult = null;
+        LibroDomain result = instance.getLibro();
+        assertEquals(expResult, result);
+     
+    }
+
+    /**
+     * Test of setLibro method, of class ReservaDomain.
+     */
+    public void testSetLibro() {
+        System.out.println("setLibro");
+        LibroDomain libro = null;
+        ReservaDomain instance = new ReservaDomain();
+        instance.setLibro(libro);
+    
+    }
+
+    /**
+     * Test of getEstado method, of class ReservaDomain.
+     */
+    public void testGetEstado() {
+        System.out.println("getEstado");
+        ReservaDomain instance = new ReservaDomain();
+        EstadoHistorico expResult = null;
+        EstadoHistorico result = instance.getEstado();
+        assertEquals(expResult, result);
+     
+    }
+    
+}
